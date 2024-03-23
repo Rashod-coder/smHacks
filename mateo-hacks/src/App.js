7d4b30e68b3c8450c90ebdb72947bd00f2b79ffb
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-03-23 14:08:51 -0700

    background

--- mateo-hacks/src/App.js
+++ mateo-hacks/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
 import Navbar from './Navbar';
-import Home from './googleSignin/Home';
+import Home from './Home';
 import Login from './logIn';
 
 function App() {
--- mateo-hacks/src/Home.js
+++ mateo-hacks/src/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
+import pic01 from "./splash1.jpeg"; // Import the image
 
 function HomePage() {
     const style = {
         height: '100vh', // Set the height to 100vh (100% of the viewport height)
+        backgroundImage: `url(${pic01})`, // Set the background image
+        backgroundSize: 'cover', // Cover the entire container with the image
+        backgroundPosition: 'center', // Center the image
     };
     
     return (
@@ -16,9 +20,7 @@ function HomePage() {
             
             <h1>Welcome to our website!</h1>
             <p>This is the homepage content.</p>
-            
         </div>
-
     );
 }
 
