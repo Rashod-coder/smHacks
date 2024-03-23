62b9ec90c78d666a08045edfc34fd61458354c10
smarkiv321 <134812432+smarkiv321@users.noreply.github.com>
2024-03-23 12:50:33 -0700

    m

--- mateo-hacks/src/Navbar.js
+++ mateo-hacks/src/Navbar.js
@@ -2,47 +2,58 @@ import React from 'react';
 
 const Navbar = ({ scrollToSection, aboutRef, projectsRef, contactRef }) => {
   return (
-    <nav className="navbar bg-body-tertiary fixed-top">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Offcanvas navbar</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-          <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-          </div>
-          <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form className="d-flex mt-3" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+    <main>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
+        <div className="container-fluid">
+          <a className="navbar-brand fs-4" href="#">Bootstrap navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="siderbar offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header text-white border-bottom">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Bootstrap Navbar</h5>
+              <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body d-flex flex-column flex-lg-row p-4 p-lg-0">
+              <ul className="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1 pe-3">
+                <li className="nav-item mx-2">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item mx-2">
+                  <a className="nav-link" href="#about">About</a>
+                </li>
+                <li className="nav-item mx-2">
+                  <a className="nav-link" href="#services">Services</a>
+                </li>
+                <li className="nav-item mx-2">
+                  <a className="nav-link" href="#contact">Contact</a>
+                </li>
+              </ul>
+              <div className="d-flex flex-column justify-content-center align-items-center gap-3">
+                <a href="#login" className="text-white">Login</a>
+                <a
+                  href="#signup"
+                  className="text-white text-decoration-none px-3 py-1 rounded-4"
+                  style={{ backgroundColor: '#f94ca4' }}
+                >Sign Up</a>
+                <form className="d-flex mt-3" role="search">
+                  <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                </form>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <section className="w-100 vh-100 d-flex flex-column justify-content-center align-items-center text-white fs-1">
+        <h1 style={{ fontSize: '1.5em' }}>Responsive Navbar</h1>
+        <h1 style={{ fontSize: '1.3em' }}>Bootstrap 5</h1>
+      </section>
+    </main>
   );
 };
 
