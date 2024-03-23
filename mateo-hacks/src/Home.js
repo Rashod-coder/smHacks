4db513e9571cf0e9fb7e5ac950a134e905b625c9
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-03-23 11:22:22 -0700

    pushing p fr

---
+++ mateo-hacks/src/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function HomePage() {
+    const style = {
+        height: '100vh', // Set the height to 100vh (100% of the viewport height)
+    };
+
+    return (
+        <div style={style}>
+            <br/>   
+            <br/>   
+            <br/>   
+            <br/>   
+            <br/>   
+            <br/>   
+            
+            <h1>Welcome to our website!</h1>
+            <p>This is the homepage content.</p>
+        </div>
+    );
+}
+
+export default HomePage;
