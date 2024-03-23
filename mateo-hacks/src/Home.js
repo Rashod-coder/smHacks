f06d60ec9862fd0e18a88b87a30e680ba18ebafe
smarkiv321 <134812432+smarkiv321@users.noreply.github.com>
2024-03-23 15:40:12 -0700

    homepage

--- mateo-hacks/src/Home.js
+++ mateo-hacks/src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import pic01 from "./splash1.jpeg"; // Import the image
+import "./Home.css"; // Import the CSS file
 
 function HomePage() {
     const style = {
@@ -8,18 +9,14 @@ function HomePage() {
         backgroundSize: 'cover', // Cover the entire container with the image
         backgroundPosition: 'center', // Center the image
     };
-    
+
     return (
         <div style={style}>
-            <br/>   
-            <br/>   
-            <br/>   
-            <br/>   
-            <br/>   
-            <br/>   
-            
-            <h1>Welcome to our website!</h1>
-            <p>This is the homepage content.</p>
+        <div className="container" style={{ backgroundImage: `url(${pic01})` }}>
+            <h1 className="title">Welcome to Boost-our-Brains!</h1>
+            <p className="paragraph">This is the homepage content.</p>
+            <p className="mission"><strong>Our Mission:</strong> We, as a young group of students, aspire to spread our idea that education is deserved by all. We want all youth, from the underpriviledged to the ambitious, to have the opportunity to gain a solid education and learn so that they can put it to practical use and be the new tech wizards of contemporary society.</p>
+        </div>
         </div>
     );
 }
