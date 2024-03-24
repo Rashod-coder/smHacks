3a567d7261b84e6d67b6cbb1a9d6cd659aa0efd5
dhruvvk14 <dhruvkkothari@gmail.com>
2024-03-23 18:38:34 -0700

    enter ur commit message

--- mateo-hacks/src/userPage.js
+++ mateo-hacks/src/userPage.js
@@ -2,15 +2,25 @@ import React from 'react';
 
 function UserPage() {
     const pageStyle = {
-        backgroundColor: 'black', // Set background color to black
-        color: 'white', // Set text color to white
+        backgroundColor: 'white', // Set background color to black
+        color: 'blue', // Set text color to white
         padding: '20px', // Add some padding for better readability
         minHeight: '100vh', // Ensure the page takes up at least the full height of the viewport
     };
 
+    const handleClick = (event) => {
+        event.preventDefault();
+        const username = document.querySelector('input[type="text"]').value;
+        
+        
+        
+      };
+
     return (
         <div style={pageStyle}>
             <h1>Hello, Adam! This is your personalized dashboard</h1>
+            <input type="text" placeholder='Place Prompt Here' required/>
+            <button onClick={handleClick} type='Submit'>Enter</button>
         </div>
     );
 }
